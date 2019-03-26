b81718ea708295e1e20c4a857fb86b4c7c0cdad5
Brian Hensley and Zsuzsanna Mangu <brian@>
2019-03-26 14:39:59 -0700

    change color of terms in list, add highlight of text and show text when clicked

---
+++ js/scripts.js
@@ -0,0 +1,77 @@
+$(function() {
+  $("#clickable1").click(function() {
+    $("#clickable1").removeClass();
+    $("#clickable1").addClass("highlight");
+    $("p#clickable").show();
+  });
+});
+
+$(function() {
+  $("#clickable2").click(function() {
+    $("#clickable2").removeClass();
+    $("#clickable2").addClass("highlight");
+    // $("p#clickable2").show();
+  });
+});
+
+$(function() {
+  $("#clickable3").click(function() {
+    $("#clickable3").addClass("highlight");
+  });
+});
+
+$(function() {
+  $("#clickable4").click(function() {
+    $("#clickable4").addClass("highlight");
+  });
+});
+
+$(function() {
+  $("#clickable5").click(function() {
+    $("#clickable5").addClass("highlight");
+  });
+});
+
+$(function() {
+  $("#clickable6").click(function() {
+    $("#clickable6").addClass("highlight");
+  });
+});
+
+$(function() {
+  $("#clickable7").click(function() {
+    $("#clickable7").addClass("highlight");
+  });
+});
+
+$(function() {
+  $("#clickable8").click(function() {
+    $("#clickable8").addClass("highlight");
+  });
+});
+
+$(function() {
+  $("#clickable9").click(function() {
+    $("#clickable9").addClass("highlight");
+  });
+});
+
+$(function() {
+  $("#clickable10").click(function() {
+    $("#clickable10").addClass("highlight");
+  });
+});
+
+
+
+
+
+// $(function() {
+//   $("li.hiddentext").click(function) {
+//     $("p.hiddentext").addClass
+//
+//   }
+//
+//
+//
+// });
